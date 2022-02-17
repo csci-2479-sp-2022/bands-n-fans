48fc27cfaab768b106f88ce06fa882629c6a947a
Joe_Robbins <sbcjoe@gmail.com>
2022-02-17 08:33:42 -0500

    added search bar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,10 +16,22 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                
             </div>
+
+
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
+                <div class="mr-10">
+                <div class="hidden space-x-8 sm:-my-px sm:mr-10 sm:flex items-center">
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out" type="submit">Search</button>
+                    </form>
+                </div>
+                </div>
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
@@ -32,6 +44,8 @@
                             </div>
                         </button>
                     </x-slot>
+
+                    
 
                     <x-slot name="content">
                         <!-- Authentication -->
