4a4ba82f8c862dfe9b7f1b1f9c7d929ba104cd13
Joe Robbins <63748021+wtjoe@users.noreply.github.com>
2022-02-20 16:28:46 -0500

    added search bar (#8)
    * added search bar
    
    * added search results mvc and updated nav layout

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,11 +23,23 @@
                 </div>
             </div>
 
+
+
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
+                <div class="mr-2">
+                <div class="hidden space-x-8 sm:-my-px sm:mr-10 sm:flex items-center">
+                    <form method="post" class="form-inline my-2 my-lg-0" action="/search-results">
+                    <!-- CSRF token? -->
+                    @csrf
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out mr-2" type="submit">Search</button>
+                    </form>
+                </div>
+                </div>
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out ml-4">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
@@ -37,6 +49,8 @@
                             </div>
                         </button>
                     </x-slot>
+
+                    
 
                     <x-slot name="content">
                         <!-- Authentication -->
