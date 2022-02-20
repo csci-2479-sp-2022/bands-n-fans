48cc2abf7883c69c908df1dff36e233e575eaa19
Joe_Robbins <sbcjoe@gmail.com>
2022-02-20 11:21:13 -0500

    added route for homepage

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->middleware(['auth'])->name('login');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
