69103a3b6a063c112b6e51a522d2862f1d36f3e6
xstreamfishn <38022840+xstreamfishn@users.noreply.github.com>
2022-02-23 20:57:55 -0500

    updated band-list.blade.php (#14)

--- resources/views/band-list.blade.php
+++ resources/views/band-list.blade.php
@@ -1,12 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <p>Band List</p>
-</body>
-</html>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Profile') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Band List
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
