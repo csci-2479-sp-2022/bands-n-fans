4c46fbfc3f2590789b42494db19898ed83bcc07b
Joe Robbins <63748021+wtjoe@users.noreply.github.com>
2022-03-16 19:23:12 -0400

    updated routes and views (#34)

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
 
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('band-list')" :active="request()->routeIs('band-list')">
+                    <x-nav-link :href="route('bands')" :active="request()->routeIs('bands')">
                         {{ __('Bands') }}
                     </x-nav-link>
                 </div>
@@ -83,7 +83,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('band-list')" :active="request()->routeIs('band-list')">
+            <x-responsive-nav-link :href="route('bands')" :active="request()->routeIs('bands')">
                 {{ __('Bands') }}
             </x-responsive-nav-link>
         </div>
