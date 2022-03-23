213605c932e88b8721c8a39b5b2e9a37ee043fec
xstreamfishn <38022840+xstreamfishn@users.noreply.github.com>
2022-03-23 08:20:16 -0400

    Issue 17 migrations (#36)
    * fixed bands route error
    
    * Completed issuue 17,  I had to change the the (/band) and the (/band/{id}) routes before I could start because it wasn't working correctly

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,11 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function band()
+    {
+        return $this->belongsToMany(Band::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
