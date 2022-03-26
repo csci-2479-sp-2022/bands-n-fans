69938753869bef5000ac8aa76cf7b8402035faaa
retrodoc <retrodoc@Retro.localdomain>
2022-03-26 14:23:20 -0400

    Added register button

--- resources/views/layouts/guestnav.blade.php
+++ resources/views/layouts/guestnav.blade.php
@@ -53,6 +53,7 @@
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
+            
 
             <div class="mt-3 space-y-1">
                 <!-- Authentication -->
