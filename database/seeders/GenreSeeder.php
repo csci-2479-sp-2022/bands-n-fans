649707aeca2d1af42ccd2ebdb9c7e5f8933786fb
James McCue <xstreamfishn@gmail.com>
2022-03-31 19:08:50 -0400

    Completed Genre seeder

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Genre;
 
 class GenreSeeder extends Seeder
 {
@@ -14,6 +15,66 @@ class GenreSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $factory = Genre::factory();
+        $factory->createMany([
+            ['name' => 'Alternative','code' => 'ALT',],
+            ['name' => 'Blues','code' => 'BLU',],
+            ['name' => 'Country','code' => 'CNT',],
+            ['name' => 'Easy Listening','code' => 'LIS',],
+            ['name' => 'Electronic','code' => 'ELC',],
+            ['name' => 'Folk','code' => 'FLK',],
+            ['name' => 'Hip Hop','code' => 'HIP',],
+            ['name' => 'Jazz','code' => 'JAZ',],
+            ['name' => 'Metal','code' => 'MET',],
+            ['name' => 'Pop','code' => 'POP',],
+            ['name' => 'R&B','code' => 'R&B',],
+            ['name' => 'Rock','code' => 'RCK',],
+            ['name' => 'Soul','code' => 'SOL',],
+        ]);
     }
 }
+
+
+
+
+/* DB::table('genres')->insert([
+    'name' => 'Alternative',
+    'code' => 'ALT',
+   ], [
+    'name' => 'Blues',
+    'code' => 'BLU',
+   ], [
+    'name' => 'Country',
+    'code' => 'CNT',
+   ], [
+    'name' => 'Easy Listening',
+    'code' => 'LIS',
+   ], [
+    'name' => 'Electronic',
+    'code' => 'ELC',
+   ], [
+    'name' => 'Folk',
+    'code' => 'FLK',
+   ], [
+    'name' => 'Hip Hop',
+    'code' => 'HPH',
+   ], [
+    'name' => 'Jazz',
+    'code' => 'JAZ',
+   ], [
+    'name' => 'Metal',
+    'code' => 'MET',
+   ], [
+    'name' => 'Pop',
+    'code' => 'POP',
+   ], [
+    'name' => 'R&B',
+    'code' => 'R&B',
+   ], [
+    'name' => 'Rock',
+    'code' => 'RCK',
+   ], [
+    'name' => 'Soul',
+    'code' => 'SOL',
+   ],
+        ); */
