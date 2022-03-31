d6b3361a4bbde3efba1e1fcd66ddc19f2dbab4a4
James McCue <xstreamfishn@gmail.com>
2022-03-31 16:56:13 -0400

    partially done-swithcing to main to take on new issue

--- resources/views/band-list.blade.php
+++ resources/views/band-list.blade.php
@@ -1,33 +1,51 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+            {{ __('Bands') }}
         </h2>
     </x-slot>
+    <x-slot name="slot">
 
-    <div class="py-12">
+
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    Band List
-
+            <div class="py-6 mt-12 grid grid-cols-2 gap-6">
+                @foreach($bands as $band)
+                <div class="">
+                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-6 bg-white border-b border-gray-200">
+                                <div class="grid grid-cols-2 gap-4">
+                                  <div>Band/Artist:</div>
+                                  <div>{{$band->name }}</div>
+                                  <div>Genre:</div>
+                                  <div>{{$band->genre}}</div>
+                                </div>
+                            </div>
+                        </div>
                 </div>
+                @endforeach
             </div>
         </div>
-    </div>
-    <div class="py-12">
+
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                   
-                <ol>@foreach($bands as $band)
-                    <li>{{$band}}</li>
-                    @endforeach</ol>
+            <div class="py-6">
+                @foreach($bands as $band)
+                <div class="mt-12">
+                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-6 bg-white border-b border-green-200">
+                                <div class="grid grid-cols-2 gap-4">
+                                  <div class="border-b-2 border-red-700">Band/Artist:</div>
+                                  <div>{{$band->name }}</div>
+                                  <div>Genre:</div>
+                                  <div>{{$band->genre}}</div>
+                                </div>
+                            </div>
+                        </div>
                 </div>
-                </div>
+                @endforeach
             </div>
         </div>
-    </div>
+
+</x-slot>
+
 </x-app-layout>
 
-                  
